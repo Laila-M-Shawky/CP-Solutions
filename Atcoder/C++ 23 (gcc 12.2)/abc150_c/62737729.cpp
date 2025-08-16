dff288047c5debd8c70da611db57e218ab761ea5
Laila Shawky <145765904+Laila-M-Shawky@users.noreply.github.com>
2025-08-16 05:00:41 +0300

    Committing files

---
+++ Atcoder/C++ 23 (gcc 12.2)/abc150_c/62737729.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Statixia ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define ll long long
+
+ll testcases = 1;
+
+int solve()
+{
+    int size;
+    cin >> size;
+    vector<int> vec1(size), vec2(size);
+    for (auto & it : vec1)
+    {
+        cin >> it;
+    }
+    for (auto & it : vec2)
+    {
+        cin >> it;
+    }
+    vector<int> numbers = vec1;
+    sort(numbers.begin(), numbers.end());
+    vector<vector<int>> permutations;
+    do
+    {
+        permutations.push_back(numbers);
+    } while (next_permutation(numbers.begin(), numbers.end()));
+    auto firstIndx = find(permutations.begin(), permutations.end(), vec1);
+    auto secondIndx = find(permutations.begin(), permutations.end(), vec2);
+    return abs(firstIndx - secondIndx);
+}
+
+int main()
+{
+    Statixia
+    //cin >> testcases;
+    while (testcases--)
+    {
+        cout << solve() << '\n';
+    }
+    return 0;
+}
+
